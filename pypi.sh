d64367a5ab3c778caa55d079603fb566fc0c8c0f
mark-summerfield <mark@qtrac.eu>
2022-04-09 13:29:03 +0100

    renaming

--- pypi.sh
+++ pypi.sh
@@ -1,3 +1,3 @@
-rm -rf build/ dist/ pxd1.egg-info/
+rm -rf build/ dist/ uxf.egg-info/
 py setup.py sdist bdist_wheel
-twine upload dist/* && rm -rf build/ dist/ pxd1.egg-info/
+twine upload dist/* && rm -rf build/ dist/ uxf.egg-info/
