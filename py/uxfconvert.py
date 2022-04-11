2352a18c481fa8f2333fd4d03c563c67bb82ec9d
mark-summerfield <mark@qtrac.eu>
2022-04-11 12:36:12 +0100

    usage message

--- py/uxfconvert.py
+++ py/uxfconvert.py
@@ -4,36 +4,44 @@
 
 import uxf
 
-# uxfconvert [-z|--compress] [-i|--indent=n]
-#	     <infile1.csv [infile2.csv ... infileN.csv]
-#	     |infile.{csv,ini,json,uxf,sqlite,toml,xml,yaml}>
-#	     [<outfile.{csv,ini,json,uxf,sqlite,toml,xml,yaml}>]
-#
-# Convert a file (or csv files) to or from UXF format.
-#
-# These options only apply to UXF output:
-# -i|--indent default 2 range 0-8
-# -z|--compress default don't compress
-#
-# To produce minimize UXF output size use -z -i0.
-#
-# Converting uxf to csv can only be done if the UXF contains a single
-# table. Converting csv to uxf produces a uxf containing a list of one
-# or more tables.
-# Converting sqlite to uxf only converts tables, so like many of the
-# possible conversions, this conversion does not round-trip.
-#
-# uxf to uxf conversions are also supported by the uxf.py module, e.g.,
-#   python3 -m uxf infile.uxf outfile.uxf
-# with the same indent and compress options.
-#
-# If an infile or outfile is given with an unrecognized suffix,
-# uxfconvert will try to guess.
 
 
 def main():
-    # TODO
-    print('uxf', uxf.VERSION, uxf.__version__)
+    # TODO use argparse?
+    # TODO make sure the suffixes work
+    print(USAGE)
+
+
+USAGE = '''\
+uxfconvert.py <infile.uxf> <outfile.{csv,ini,json,sqlite,xml}>
+uxfconvert.py [-z|--compress] [-i|--indent=N] [-f|--fieldnames]
+    <infile.{csv,ini,json,sqlite,xml}> <outfile.uxf>
+uxfconvert.py [-z|--compress] [-i|--indent=N] [-f|--fieldnames]
+    <infile1.csv> [infile2.csv ... infileM.csv] <outfile.uxf>
+
+Options:
+  -i | --indent     default 2, range 0-8
+  -z | --compress   default don't compress
+  -f | --fieldnames default no fieldnames (only applies to csv infiles)
+
+To produce the smallest uxf output use options: -z -i0.
+
+If multiple csv files are given as infiles, the outfile will be a uxf
+containing a list of tables. If the fieldnames option is given the first
+line of each csv file will be assumed to be field names rather than values.
+
+Converting from uxf to csv can only be done if the uxf contains a single
+table.
+
+Converting sqlite to uxf only converts tables, so like many of the
+possible conversions, this conversion does not round-trip.
+
+Converting xml to uxf only works for the xml that is output by
+uxfconvert.py when converting from uxf to xml.
+
+uxf to uxf conversions are also supported by the uxf.py module, e.g.,
+  python3 -m uxf infile.uxf outfile.uxf
+with the same indent and compress options.'''
 
 
 if __name__ == '__main__':
