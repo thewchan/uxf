464d62ce9a3382927a292b1cdff46783de390db3
mark-summerfield <mark@qtrac.eu>
2022-04-12 13:57:34 +0100

    tiny

--- st.sh
+++ st.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-tokei -f -tPython -esetup.py -eepd2pxd.py -eregression.py
+tokei -f -tPython -esetup.py -eepd2uxf.py -eregression.py
 unrecognized.py -q
 python3 -m flake8 --ignore=W504,E261,E303 .
 python3 -m vulture . 
