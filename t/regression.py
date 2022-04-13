53321e0f9b957c559159262a05244d518bb0273b
mark-summerfield <mark@qtrac.eu>
2022-04-13 07:54:07 +0100

    improved regression.py

--- t/regression.py
+++ t/regression.py
@@ -7,49 +7,61 @@ import os
 import re
 import subprocess
 import sys
+import time
 
 os.chdir(os.path.dirname(__file__))
 
 
 def main():
-    uxf, uxfconvert = get_exes()
+    uxf, uxfconvert, verbose = get_config()
     cleanup()
+    t = time.monotonic()
     uxffiles = sorted((name for name in os.listdir('.')
                        if os.path.isfile(name) and name.endswith('.uxf')),
                       key=by_number)
-    total, ok = test_uxf(uxf, uxffiles)
-    total, ok = test_uxfconvert(uxfconvert, uxffiles, total, ok)
-    print(f'{ok}/{total}', 'All OK' if total == ok else 'FAIL')
+    total, ok = test_uxf(uxf, uxffiles, verbose=verbose)
+    total, ok = test_uxfconvert(uxfconvert, uxffiles, total, ok,
+                                verbose=verbose)
     if total == ok:
+        t = time.monotonic() - t
+        print(f'{ok}/{total} All OK ({t:.3f} sec)')
         cleanup()
+    else:
+        print(f'{ok}/{total} FAIL')
 
 
-def get_exes():
+def get_config():
     uxf = '../py/uxf.py'
     uxfconvert = '../py/uxfconvert.py'
-    if len(sys.argv) > 1:
-        if sys.argv[1] in {'-h', '--help'}:
+    verbose = False
+    for arg in sys.argv[1:]:
+        if arg in {'-h', '--help'}:
             raise SystemExit('usage: regression.py [path/to/uxf-exe]')
-        uxf = sys.argv[1]
-        uxfconvert = os.path.join(os.path.dirname(uxf, 'uxfconvert.py'))
-    return uxf, uxfconvert
+        elif arg in {'-v', '--verbose', 'verbose'}:
+            verbose = True
+        else:
+            uxf = arg
+            uxfconvert = os.path.join(os.path.dirname(uxf, 'uxfconvert.py'))
+    return uxf, uxfconvert, verbose
 
 
-def test_uxf(uxf, uxffiles):
+def test_uxf(uxf, uxffiles, *, verbose):
     total = ok = 0
     for name in uxffiles:
         total += 1
         actual = f'actual/{name}'
         expected = f'expected/{name}'
-        reply = subprocess.call([uxf, name, actual])
+        cmd = [uxf, name, actual]
+        reply = subprocess.call(cmd)
+        cmd = ' '.join(cmd)
         if reply != 0:
-            print(f'uxf: {name} FAIL could not output {actual}')
+            print(f'{cmd} • FAIL (execute)')
         else:
-            ok += compare('uxf', name, actual, expected)
+            ok += compare(cmd, name, actual, expected, verbose=verbose)
     return total, ok
 
 
-def test_uxfconvert(uxfconvert, uxffiles, total, ok):
+def test_uxfconvert(uxfconvert, uxffiles, total, ok, *, verbose):
     files = [(name, name.replace('.uxf', '.json')) for name in uxffiles]
     files += [('t1.uxf', 't1.csv'), ('t2.uxf', 't2.csv'),
               ('0.csv', '0.uxf'), ('1.csv', '1.uxf'), ('2.csv', '2.uxf')]
@@ -57,35 +69,39 @@ def test_uxfconvert(uxfconvert, uxffiles, total, ok):
     for infile, outfile in files:
         total += 1
         actual = f'actual/{outfile}'
-        args = ([uxfconvert, '-f', infile, actual] if infile == '1.csv' else
-                [uxfconvert, infile, actual])
-        reply = subprocess.call(args)
+        cmd = ([uxfconvert, '-f', infile, actual] if infile == '1.csv' else
+               [uxfconvert, infile, actual])
+        reply = subprocess.call(cmd)
+        cmd = ' '.join(cmd)
         if reply != 0:
-            print(f'uxfconvert: {infile} FAIL could not output {actual}')
+            print(f'{cmd} • FAIL (execute)')
         else:
             expected = f'expected/{outfile}'
-            ok += compare('uxfconvert', infile, actual, expected)
+            ok += compare(cmd, infile, actual, expected, verbose=verbose)
     total += 1
     actual = 'actual/1-2-csv.uxf'
     infile = '1.csv 2.csv'
-    reply = subprocess.call([uxfconvert, '-f', '1.csv', '2.csv', actual])
+    cmd = [uxfconvert, '-f', '1.csv', '2.csv', actual]
+    reply = subprocess.call(cmd)
+    cmd = ' '.join(cmd)
     if reply != 0:
-        print(f'uxfconvert: {infile} FAIL could not output {actual}')
+        print(f'{cmd} • FAIL (execute)')
     else:
         expected = 'expected/1-2-csv.uxf'
-        ok += compare('uxfconvert', infile, actual, expected)
+        ok += compare(cmd, infile, actual, expected, verbose=verbose)
     return total, ok
 
 
-def compare(cmd, infile, actual, expected):
+def compare(cmd, infile, actual, expected, *, verbose):
     try:
         if filecmp.cmp(actual, expected, False):
-            print(f'{cmd}: {infile} → {actual} OK')
+            if verbose:
+                print(f'{cmd} • {infile} → {actual} OK')
             return 1
         else:
-            print(f'{cmd}: {infile} FAIL {expected} != {actual}')
+            print(f'{cmd} • FAIL (compare)')
     except FileNotFoundError:
-        print(f'{cmd}: {infile} FAIL missing: {expected}')
+        print(f'{cmd} • FAIL (missing {expected!r})')
     return 0
 
 
