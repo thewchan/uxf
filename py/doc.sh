85cc6e83570d3ae4490404f1900b7741c0d8bd5c
mark-summerfield <mark@qtrac.eu>
2022-05-19 21:34:39 +0100

    tiny

--- py/doc.sh
+++
@@ -1,6 +0,0 @@
-cd ~/app/uxf/py
-pydoc -w uxf > /dev/null
-rm -f ~/qtrac/www-live/static/uxf-api.html
-mv uxf.html ~/qtrac/www-live/static/uxf-api.html
-echo wrote ~/qtrac/www-live/static/uxf-api.html
-cd ..
