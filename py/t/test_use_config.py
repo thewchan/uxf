3fc908e4bf3f93f233cf1fab0b019cb92a935d2b
mark-summerfield <mark@qtrac.eu>
2022-05-24 13:38:12 +0100

    improved

--- py/t/test_use_config.py
+++ py/t/test_use_config.py
@@ -9,9 +9,9 @@ config data.
 See ../../testdata/use_config1.conf
 '''
 
-import sys
+import enum
 import os
-
+import sys
 
 try:
     PATH = os.path.abspath(os.path.dirname(__file__))
@@ -34,9 +34,17 @@ def main():
     print(f'total={total} ok={ok}')
 
 
+class Symbols(enum.IntEnum):
+    DECIMAL = 1
+    ROMAN = 2
+
+
 class Config:
 
     def __init__(self, filename=None):
+        '''All the data is held in self.uxo in UXF format. However, the
+        class' properties get and set using the appropriate format (e.g.,
+        .symbols as a Symbols enum)'''
         self.filename = filename
         initiallyvisible = uxf.TClass('initiallyvisible',
                                       (uxf.Field('min', 'int'),
@@ -63,83 +71,65 @@ class Config:
     def load(self, filename=None):
         if filename is not None:
             self.filename = filename
-        try: # can't `self.uxd = uxf.load(…)` because we want to validate
-            uxo = uxf.load(self.filename)
-            self.uxo.comment = uxo.comment
-            self.uxo.comment = uxo.comment
-            self.uxo.data.comment = uxo.data.comment
-            self.uxo.data.ktype = uxo.data.ktype
-            self.uxo.data.vtype = uxo.data.vtype
-            general = uxo.data.get('general')
-            if general is not None:
-                for value in general:
-                    print(type(value), value)
+        try:
+            tclasses = self.uxo.tclasses
+            self.uxo = uxf.load(self.filename)
+            self.uxo.tclasses.update(tclasses) # make sure they're all there
         except OSError as err:
             print(f'Failed to load configuration file: {err}. '
                   'Will try to create a new one on exit.')
 
 
-#    def save(self, filename=None):
-#        if filename is not None:
-#            self.filename = filename
-#        # It would be far easier to store the map item as
-#        # <symbols> <DECIMAL> or <symbols> <ROMAN> but I wanted an enum eg.
-#        try:
-#            symbol = self._uxd.data['symbols']
-#            self._uxd.data['symbols'] = uxf.Table(self._symbol_tclass,
-#                                                  records=(symbol.name,))
-#            self._uxd.dump(self.filename)
-#        finally:
-#            self._uxd.data['symbols'] = symbol
-#
-#
-#    def update(self, d):
-#        # We store the Symbols as an enum but load/save it as a UType
-#        for key, value in d.items():
-#            setattr(self, key, value)
-#
-#
-#    def __getitem__(self, name):
-#        return self.__dict__['_uxd'].data[name]
-#
-#
-#    def __getattr__(self, name):
-#        if name in self.__dict__:
-#            return self.__dict__[name]
-#        if name in self.__dict__['_uxd'].data:
-#            return self.__dict__['_uxd'].data[name]
-#        raise AttributeError(f'{self.__class__.__name__!r} object has no '
-#                             f'attribute {name!r}')
-#
-#
-#    def __setattr__(self, name, value):
-#        if name in self.__dict__:
-#            self.__dict__[name] = value
-#            return
-#        if name not in self._uxd.data:
-#            raise AttributeError(f'{self.__class__.__name__!r} object '
-#                                 f'has no attribute {name!r}')
+    def save(self, filename=None):
+        if filename is not None:
+            self.filename = filename
+        self.uxo.dump(self.filename)
+
+
+    @property
+    def symbols(self):
+        symbol, _ = self._symbol_and_index()
+        if symbol is not None:
+            return symbol
+        self.uxo.data['general'].append(
+            uxf.Table(self.uxo.tclasses['Decimal']))
+        return Symbols.DECIMAL
+
+
+    def _symbol_and_index(self):
+        self._symbol_for_name = {symbol.name.upper(): symbol
+                                 for symbol in Symbols}
+        for i in range(len(self.uxo.data['general'])):
+            value = self.uxo.data['general'][i]
+            ttype = value.ttype.upper()
+            symbol = self._symbol_for_name.get(ttype)
+            if symbol is not None:
+                return symbol, i
+        return None, -1
+
+
+    @symbols.setter
+    def symbols(self, value):
+        symbol, i = self._symbol_and_index()
+        if symbol is not None:
+            if symbol is value:
+                return # unchanged
+        if i == -1:
+            i = len(self.uxo.data['general'])
+            self.uxo.data['general'].append(None)
+        ttype = value.name.capitalize()
+        self.uxo.data['general'][i] = uxf.Table(self.uxo.tclasses[ttype])
+
+
+    # TODO add a property getter/setter for every config item, validating &
+    # converting types on-demand
+
 #        elif (name in {'maxinitiallyvisible', 'mininitiallyvisible'} and
 #                not (isinstance(value, int) and (9 <= value <= 72))):
 #            return # ignore bad value
 #        elif (name == 'fontsize' and not (isinstance(value, int) and
 #              (8 <= value <= 36))):
 #            return # ignore bad value
-#        elif name == 'symbols':
-#            if not isinstance(value, Symbols): # use Symbols value as-is
-#                if (not isinstance(value, uxf.Table) or
-#                        value.ttype != 'Symbol'):
-#                    return # ignore bad value
-#                record = value.first
-#                if record is None:
-#                    return # ignore missing value
-#                for symbol in Symbols:
-#                    if symbol.name == record.name:
-#                        value = symbol
-#                        break
-#                else:
-#                    return # ignore bad value
-#        self._uxd.data[name] = value
 
 
 if __name__ == '__main__':
