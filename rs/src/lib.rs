a5ac9dfdb5da4226cbb12166954e0a4408aaa3f9
mark-summerfield <mark@qtrac.eu>
2022-07-11 07:43:47 +0100

    preparing to start Rust uxf lib

---
+++ rs/src/lib.rs
@@ -0,0 +1,11 @@
+// Copyright © 2022 Mark Summerfield. All rights reserved.
+// License: GPLv3
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
